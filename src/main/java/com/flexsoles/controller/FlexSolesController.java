af9df567ef2b398d6be718d2e191d12877a8476f
DavidChiscano <chiscano94@gmail.com>
2021-10-18 19:27:03 +0200

    V0.1

--- src/main/java/com/flexsoles/controller/FlexSolesController.java
+++ src/main/java/com/flexsoles/controller/FlexSolesController.java
@@ -1,5 +1,24 @@
 package com.flexsoles.controller;
 
-public class FlexSolesController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+public class FlexSolesController {
+	//GET METHODS	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String getQuiz() {
+		return "index";
+	}
+	@RequestMapping(value = "/producto/idProducto", method = RequestMethod.GET)
+	public String getIdProducto() {
+		return "idProducto";
+	}
+	@RequestMapping(value = "/producto/crear", method = RequestMethod.GET)
+	public String getCrearProducto() {
+		return "crearProducto";
+	}
+	@RequestMapping(value = "/producto/buscar", method = RequestMethod.GET)
+	public String getBuscarProducto() {
+		return "buscarProducto";
+	}
 }
