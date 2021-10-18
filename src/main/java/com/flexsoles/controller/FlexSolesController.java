5a32de18ca9f3e6725cdaef36b087fe4ebd1add9
DavidChiscano <chiscano94@gmail.com>
2021-10-18 20:03:40 +0200

    v0.01

--- src/main/java/com/flexsoles/controller/FlexSolesController.java
+++ src/main/java/com/flexsoles/controller/FlexSolesController.java
@@ -1,12 +1,25 @@
 package com.flexsoles.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class FlexSolesController {
+	
+	//VARIABLES PARA EL FORMULARIO CREAR PRODUCTO
+	
+	int id, descuento;
+	String titulo, descripcion;
+	double precio;
+	
 	//GET METHODS	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String getQuiz() {
+	public String getIndex() {
 		return "index";
 	}
 	@RequestMapping(value = "/producto/idProducto", method = RequestMethod.GET)
@@ -20,5 +33,35 @@ public class FlexSolesController {
 	@RequestMapping(value = "/producto/buscar", method = RequestMethod.GET)
 	public String getBuscarProducto() {
 		return "buscarProducto";
+	}
+	@RequestMapping(value = "/producto/borrar/idProducto", method = RequestMethod.GET)
+	public String getBorrarIdProducto() {
+		return "borrarIdProducto";
+	}
+	
+	//MAPPING
+	@GetMapping("/producto/crear")
+	public String process(Model model, HttpSession session, HttpServletRequest request) {
+	    id = (int)session.getAttribute("id");
+	    titulo = (String)session.getAttribute("titulo");
+	    descripcion = (String)session.getAttribute("descripcion");
+	    precio = (Double)session.getAttribute("precio");
+	    descuento = (int)session.getAttribute("descuento");
+	    
+	    
+	    return "crearProducto";
+	}
+	
+	
+	//POST METHODS
+	@RequestMapping(value = "/producto/crear", method = RequestMethod.POST)
+	public String quiz(@RequestParam int id, String titulo,String descripcion, double precio, int descuento,HttpServletRequest request) {
+		request.getSession().setAttribute("id", id);
+		request.getSession().setAttribute("titulo", titulo);
+		request.getSession().setAttribute("descripcion", descripcion);
+		request.getSession().setAttribute("precio", precio);
+		request.getSession().setAttribute("descuento", descuento);
+		
+		return "redirect:/index";
 	}
 }
